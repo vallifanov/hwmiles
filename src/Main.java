546ec161f15b796d5ffff9375b02512a851ee6a6
vallifanov <vallifanov@gmail.com>
2022-03-30 18:13:59 +1200

    автоформатирование

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-//        Входные данные
+        //        Входные данные
         int amount = 14750;
         int one_mile = 20;
 //        Логика процесса
         int bonus = amount / one_mile;
-                System.out.println(bonus);
+        System.out.println(bonus);
     }
 }
